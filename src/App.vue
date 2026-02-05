98f6a37dd59c60ff61759c3fed6f5fdb2d153174
Kebebush Merga <kebebushmerga24@gmail.com>
2026-02-05 04:09:56 +0300

    updated app.vue in the script and template to demonestrate interpolation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import HelloWorld from "./components/HelloWorld.vue";
 import TheWelcome from "./components/TheWelcome.vue";
+const name = "SPEC Developer";
 </script>
 
 <template>
   <h1>Hello SPEC Developers</h1>
+  <br />
+  <h1>Hello {{ name }}</h1>
 </template>
 
 <style scoped>
